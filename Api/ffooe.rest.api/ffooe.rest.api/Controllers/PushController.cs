5259911406f488dcbfc36e82c27e9cc82b5ee1f5
Mag. Helmut Hackl <Helmut.Hackl@engel.at>
2024-07-12 14:19:59 +0200

    Added callback fopr push messages

---
+++ Api/ffooe.rest.api/ffooe.rest.api/Controllers/PushController.cs
@@ -0,0 +1,97 @@
+﻿using ffooe.db.context;
+using ffooe.rest.api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace ffooe.rest.api.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/v1.0/[controller]")]
+    public class PushController : ControllerBase
+    {
+        private readonly ILogger<PushController> _logger;
+        private readonly FFOOEContext _context;
+        public PushController(ILogger<PushController> logger, FFOOEContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        [HttpPost("callback")]
+        public PushSaferResponse PostCallback([FromForm] string json)
+        {
+            var pushSaferResponse = JsonSerializer.Deserialize<PushSaferResponse>(WebUtility.HtmlDecode(json));
+            try
+            {
+                _logger.LogInformation("----------------------------------------------------------");
+                _logger.LogInformation("Callback hit!");
+                _logger.LogInformation("----------------------------------------------------------");
+                _logger.LogInformation(pushSaferResponse.Action);         // add-device
+                _logger.LogInformation(pushSaferResponse.Id);             // device id
+                _logger.LogInformation(pushSaferResponse.Name);           // registered name == Personal Guid for PushSafer
+                _logger.LogInformation(pushSaferResponse.Guest);          // 1 == Gast
+
+                if (!Action(pushSaferResponse))
+                {
+                    // ?? why have I done this ?? RemoveDevice(pushSaferResponse.Id);
+                    return pushSaferResponse;
+                }
+                if (pushSaferResponse.Action == "delete-device")
+                {
+                    if (pushSaferResponse.Id != null) // Delete device from Database DeletePushDevice(pushSaferResponse.Id
+                    {
+                        _logger.LogInformation($"Device {pushSaferResponse.Id} removedfrom 'PushSaferDevice' Table");
+                    }
+                    else
+                    {
+                        _logger.LogError($"Could not remove device {pushSaferResponse.Id} from 'PushSaferDevice' Table");
+                    }
+                }
+                if (pushSaferResponse.Action == "add-device")
+                {
+                    // Add device to db
+                    _logger.LogInformation($"Adding device {pushSaferResponse.Id} to database");
+                }
+                if (pushSaferResponse.Action == "answer transmitted")
+                {
+                    // Callback for Answers in Push message
+
+                }
+                return pushSaferResponse;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.InnerException?.ToString());
+                _logger.LogError(ex.StackTrace);
+                return pushSaferResponse;
+            }
+
+        }
+        private bool Action(PushSaferResponse response)
+        {
+            try
+            {
+                if (response.Action == "add-device" || response.Action == "delete-device" || response.Action == "answer transmitted")
+                {
+                    return true;
+                }
+                _logger.LogError($"Action {response.Action} is not handled");
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                _logger.LogError(ex.InnerException?.ToString());
+                _logger.LogError(ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ Api/ffooe.rest.api/ffooe.rest.api/Models/PushSaferResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ffooe.rest.api.Models
+{
+    public class PushSaferResponse
+    {
+        [JsonPropertyName("action")]
+        public string Action { get; set; }
+
+        [JsonPropertyName("answer")]
+        public string Answer { get; set; }
+
+        [JsonPropertyName("available")]
+        public int Available { get; set; }
+
+        [JsonPropertyName("group")]
+        public string Group { get; set; }
+
+        [JsonPropertyName("guest")]
+        public string Guest { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("message_id")]
+        public int MessageId { get; set; }
+
+        [JsonPropertyName("message_ids")]
+        public string MessageIds { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("success")]
+        public string Success { get; set; }
+    }
+}
